d107e730e8ddc9f3f0babb20e3c87df931148030
Furkan <58880808+furkanabanoz@users.noreply.github.com>
2021-10-10 17:27:11 +0300

    odev18

--- odev18.java
+++ odev18.java
@@ -2,24 +2,29 @@ import java.util.Scanner;
 
 public class odev18 {
     public static void main(String[] args) {
-        int getNumber,sum =0;
+        Scanner input = new Scanner(System.in);
+        int getNumber,sum=0;
 
-        do {
-            Scanner input = new Scanner(System.in);
-            System.out.printf("lutfen sayi giriniz : ");
-            getNumber= input.nextInt();
-            if (getNumber%2==0)
-            {
-                if (getNumber%4==0)
-                    sum+=getNumber;
-            }
+        System.out.printf("lutfen bir sayi giriniz ");
+        getNumber=input.nextInt();
 
-        }
-        while (getNumber%2==0);
+        while (getNumber%2==0)
         {
-            System.out.printf("girilen sayilarin toplami : %d dir",sum);
-
+            if (getNumber%4==0)
+            {
+                sum += getNumber;
+                System.out.printf("toplamlari : %d\n",sum);
+            }
+            System.out.printf("lutfen bir sayi giriniz ");
+            getNumber=input.nextInt();
         }
+        if (getNumber%2!=0)
+        {
+            System.out.println("\n\n\ntoplam = "+sum);
+            System.out.println("program kapatiliyor...");
+        }
+
+
     }
 
 }
